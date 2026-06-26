a5866738afa082ff2aba3909de9511679a513dc9
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-06-26 16:51:28 -0400

    Friday 4:51PM

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let careNotes = [
+  { id: 1, plantId: 1, note: "Needs water every 2 weeks." },
+  { id: 2, plantId: 1, note: "Tolerates low light well." },
+  { id: 3, plantId: 3, note: "Loves humidity." },
+];
+
+let nextNoteId = 4;
+
 let plants = [
   {
     id: 1,
@@ -49,26 +57,40 @@ app.get("/api/plants", (req, res) => {
 // but if I put the URL in without a .query, as long as it is the proper URL, the page will just load normally
 
 app.get("/api/plants/:id", async (req, res) => {
-    const plant = plants.find((plant) => {
-    return plant.id === Number(req.params.id);
-  });
-  
+  const plant = plants.find((plant) => plant.id === Number(req.params.id));
+
   if (!plant) {
-    await new Promise((resolve) => setTimeout(resolve, 500)
-    ).then(
-    res.status(404).send("Plant Not Found!!!")
-    );}
+    await new Promise(resolve => setTimeout(resolve, 500));
+    return res.status(404).send("Plant Not Found!!!");
+  }
 
-  await new Promise((resolve) => setTimeout(resolve, 500)
-  ).then(
-  res.json(plant)
-  );
+  await new Promise(resolve => setTimeout(resolve, 500));
+  res.json(plant);
 });
-
 // Explain: req.params.id is always a string. Why do you need to wrap it in Number() before comparing it to a plant's id?
 // Because === is type specific and if the types being compared are different even if they are the same number the condition will return false
 // Explain: What happens if you remove await from in front of the delay? Does the route still work the same way?
 // No, The route will still work, but it will no longer wait for the promise it will just run immediately
+
+app.get("/api/plants/:plantId/notes", (req, res) => {
+  const plantId = Number(req.params.plantId);
+    
+  const plant = plants.find((plant) => plant.id === plantId);
+
+  if (!plant) {
+    return res.status(404).send("Plant Not Found!!!");
+  }
+  
+  const note = careNotes.filter((note) => note.plantId === plantId);
+  res.json(note);
+
+  // Explain: What does :plantId represent in this URL? Why is it a param, and notes isn't?
+  // plantId represents the car note with the same Id as the plant whose Id matches. 
+  // It is a parameter and note is not because the number is dynamic and changes and the notes is static and will always say the word note.
+  // ex: api/plants/2/note, 
+  //     api/plants/3/note,
+  //     api/plants/4/note
+});
 
 app.post("/api/plants/", (req, res) => {
   const { name, type, sunlight, watered } = req.body;
@@ -82,6 +104,17 @@ app.post("/api/plants/", (req, res) => {
   nextId++;
   plants.push(newPlant);
   res.status(201).json(newPlant);
+});
+
+app.post("/api/plants/:plantId/notes", (req, res) => {
+  const { note } = req.body;
+  const newCareNote = {
+    id: nextNoteId,
+    note
+  };
+  nextNoteId++;
+  careNotes.push(newCareNote);
+  res.status(201).json(newCareNote);
 });
 
 app.patch("/api/plants/:id", (req, res) => {
